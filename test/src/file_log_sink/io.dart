bc758dbc182e045e213f3c1716204477f42c4047
FMotalleb <fmotalleb@gmail.com>
2023-05-29 11:06:41 +0330

    [Test]<FileLogSink>

---
+++ test/src/file_log_sink/io.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:hemend_async_log_recorder/src/contracts/typedefs.dart';
+import 'package:hemend_async_log_recorder/src/log_sinks/file_log_sink/file_log_sink_io.dart';
+import 'package:hemend_logger/hemend_logger.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'io.mocks.dart';
+
+// Create a mock for the File and IOSink
+@GenerateMocks([File, IOSink])
+void main() {
+  group('FileLogSink.IO', () {
+    late FileLogSink fileLogSink;
+    late RecordStringify simpleStringify;
+    late MockFile mockFile;
+    late MockIOSink mockSink;
+
+    setUp(() {
+      simpleStringify = (record) => record.message;
+      mockFile = MockFile();
+      mockSink = MockIOSink();
+
+      // Mock the file initialization
+      when(
+        mockFile.openWrite(mode: anyNamed('mode')),
+      ).thenReturn(mockSink);
+      // Mock the Sink disposition
+      when(
+        mockSink.close(),
+      ).thenAnswer((_) async => null);
+      // Mock the sink write method
+      when(
+        mockSink.add(any),
+      ).thenReturn(null);
+
+      // Create the FileLogSink instance for each test case
+      fileLogSink = FileLogSink.fromFile(
+        stringify: simpleStringify,
+        file: mockFile,
+      );
+    });
+
+    test('add method should add log record to the controller', () async {
+      // Create fake Log record
+      final mockLogRecord = LogRecordEntity(
+        message: 'test message',
+        error: null,
+        level: 800,
+        loggerName: 'TestLogger',
+        stackTrace: null,
+        time: DateTime.now(),
+        zone: null,
+      );
+
+      // Act
+      fileLogSink.add(mockLogRecord);
+      final expectedMessage = 'test message'.codeUnits;
+      // Assert
+      // This method is called after internal stream controller received record
+      // thus this method may get called after an unknown duration
+      await untilCalled(mockSink.add(any));
+      verify(mockSink.add(expectedMessage)).called(1);
+    });
+
+    test('close method should close the controller sink', () async {
+      // Act
+      await fileLogSink.close();
+
+      // Assert
+      expect(fileLogSink.isClosed, true);
+    });
+
+    // Add more test cases as needed
+
+    tearDown(() {
+      // Close the file or perform any necessary cleanup
+    });
+  });
+}
