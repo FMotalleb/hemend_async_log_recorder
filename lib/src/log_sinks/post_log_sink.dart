6cb1fcdc105d79d52e293ce3abf46d8704c07532
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2023-05-29 22:19:30 +0330

    [Feat]<PostLogger>: Concurrency

--- lib/src/log_sinks/post_log_sink.dart
+++ lib/src/log_sinks/post_log_sink.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:hemend_async_log_recorder/src/contracts/log_sink.dart';
 import 'package:hemend_async_log_recorder/src/contracts/typedefs.dart';
 import 'package:hemend_logger/hemend_logger.dart';
+import 'package:pool/pool.dart';
 
 /// {@template log-sink}
 /// {@macro log-sink-abstraction}
@@ -13,27 +14,56 @@ import 'package:hemend_logger/hemend_logger.dart';
 class PostLogSink extends ILogSink {
   /// {@macro log-sink}
   ///
-  /// * serializer: the method used to serialize the record to json
-  /// *
+  /// * `serializer` parameter is of type RecordSerializer and is required.
+  /// It specifies the serializer responsible for converting log records into
+  /// the desired format before sending them.
+  ///
+  /// * `requestUrl` parameter is a required String that represents the URL where
+  /// the log records will be sent.
+  ///
+  /// * `recordMethod` parameter is of type PostMethod and is required.
+  /// It indicates the HTTP method to be used when sending the log records.
+  /// It is likely to be a POST method in this case.
+  ///
+  /// * `concurrency` parameter is an integer that indicates the maximum number of
+  /// requests that can be launched simultaneously.
+  /// This parameter is crucial for managing the load on the server and
+  /// controlling the rate at which log records are sent.
+  ///
+  /// * `concurrencyWaitTime` parameter is an optional Duration that specifies
+  /// the amount of time to wait before launching new requests when the maximum
+  /// concurrency limit is reached. This parameter helps prevent overwhelming
+  /// the server with too many simultaneous requests.
   PostLogSink({
     required RecordSerializer serializer,
     required String requestUrl,
     required PostMethod recordMethod,
-  }) : _controller = StreamController() {
+    required int concurrency,
+    required Duration? concurrencyWaitTime,
+  })  : _controller = StreamController(),
+        _concurrentPool = Pool(
+          concurrency,
+          timeout: concurrencyWaitTime,
+        ) {
     _initSink(serializer, recordMethod, requestUrl);
   }
-
+  final Pool _concurrentPool;
   void _initSink(
     RecordSerializer serializer,
     PostMethod recordMethod,
     String requestUrl,
   ) {
     _controller.stream.map(serializer).listen(
-          (record) => recordMethod(
-            requestUrl,
-            record,
-          ),
+      (record) async {
+        final resource = await _concurrentPool.withResource(
+          () => recordMethod,
         );
+        await resource(
+          requestUrl,
+          record,
+        );
+      },
+    );
   }
 
   final StreamController<LogRecordEntity> _controller;
