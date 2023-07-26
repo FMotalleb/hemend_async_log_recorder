ed657075ee2afb4c5e9fc7a51fabad8189d4d5b8
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-07-26 10:42:10 +0330

    [Minor]

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,5 @@
 import 'package:hemend_async_log_recorder/hemend_async_log_recorder.dart';
 import 'package:hemend_logger/hemend_logger.dart';
-import 'package:logging/logging.dart';
 
 void main() async {
   Logger.root.level = Level.ALL;
